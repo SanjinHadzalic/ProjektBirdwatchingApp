188e4b6ea9cf2c569bfe56f2eb0fcfcda9c948c3
Sanjin Hadzalic <sh14.sanjin@yahoo.com>
2023-05-11 16:44:58 +0200

    feat: add new material in BirdMediaController
    This feat will add Turdus merula ImageView to BirdMediaController

--- src/main/java/com/example/projektbirdwatchingapp/BirdsMediaController.java
+++ src/main/java/com/example/projektbirdwatchingapp/BirdsMediaController.java
@@ -1,39 +1,55 @@
 package com.example.projektbirdwatchingapp;
 
-import hr.java.vjezbe.entiteti.BirdUnos;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Side;
 import javafx.scene.Scene;
-import javafx.scene.chart.PieChart;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
-import org.w3c.dom.events.MouseEvent;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.example.projektbirdwatchingapp.LoginController.odabraniUser;
 
-public class BirdsMediaController {
+public class BirdsMediaController  {
     MediaPlayer mediaPlayer;
 
+    FileInputStream input;
+
+    {
+        try {
+            input = new FileInputStream("src/main/resources/images/Tmerula.jpg");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    Image kos = new Image(input);
     @FXML
-    void play(ActionEvent event){
+    ImageView kos2 = new ImageView(kos);
+
+
+    public static void showBirdsMedia() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("birdsMedia.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        scene.getStylesheets().add("main.css");
+        Application.getMainStage().setTitle("Birds media");
+        Application.getMainStage().setScene(scene);
+        Application.getMainStage().show();
+    }
+    @FXML
+    void playKos(ActionEvent event){
         File mediaFile = new File("src/main/resources/music/Tmerula.mp3");
         playHitSound(mediaFile);
     }
 
     @FXML
-    void stopSound(ActionEvent event){
+    void stopSoundAll(ActionEvent event){
         mediaPlayer.stop();
     }
 
@@ -42,14 +58,6 @@ public class BirdsMediaController {
         mediaPlayer = new MediaPlayer(media);
         mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
         mediaPlayer.play();
-    }
-    public static void showBirdsMedia() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("birdsMedia.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        scene.getStylesheets().add("main.css");
-        Application.getMainStage().setTitle("Birds media");
-        Application.getMainStage().setScene(scene);
-        Application.getMainStage().show();
     }
 
     public void natragButtonClicked() throws IOException {
