7cb6401e62b102e5de6c1c3f4e9b920f36dd1946
Sanjin Hadzalic <sh14.sanjin@yahoo.com>
2023-06-21 23:49:06 +0200

    refactor: add Platform.runLater() in BirdsMediaController and ExceptionHandler when exiting scene

--- src/main/java/com/example/projektbirdwatchingapp/BirdsMediaController.java
+++ src/main/java/com/example/projektbirdwatchingapp/BirdsMediaController.java
@@ -1,5 +1,6 @@
 package com.example.projektbirdwatchingapp;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +9,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -16,9 +19,10 @@ import java.io.IOException;
 
 import static com.example.projektbirdwatchingapp.LoginController.odabraniUser;
 
-public class BirdsMediaController  {
+public class BirdsMediaController {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
     MediaPlayer mediaPlayer;
-
+    private boolean isPlaying = false;
     FileInputStream inputKosStream;
     FileInputStream inputSkanjacStream;
     FileInputStream inputZebaStream;
@@ -31,8 +35,8 @@ public class BirdsMediaController  {
             inputKosStream = new FileInputStream("src/main/resources/images/Tmerula.jpg");
             inputSkanjacStream = new FileInputStream("src/main/resources/images/Tmerula.jpg");
             inputZebaStream = new FileInputStream("src/main/resources/images/Finch.jpg");
-            inputVjetrusaStream=new FileInputStream("src/main/resources/images/kestrel.png");
-            inputRobinStream=new FileInputStream("src/main/resources/images/Robin.jpg");
+            inputVjetrusaStream = new FileInputStream("src/main/resources/images/kestrel.png");
+            inputRobinStream = new FileInputStream("src/main/resources/images/Robin.jpg");
             inputDivljaPatkaStream = new FileInputStream("src/main/resources/images/mallard.jpg");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -42,9 +46,9 @@ public class BirdsMediaController  {
     @FXML
     ImageView kosImageView = new ImageView(new Image(inputKosStream));
     @FXML
-    ImageView skanjacImageView=new ImageView(new Image(inputSkanjacStream));
+    ImageView skanjacImageView = new ImageView(new Image(inputSkanjacStream));
     @FXML
-    ImageView zebaImageView=new ImageView(new Image(inputZebaStream));
+    ImageView zebaImageView = new ImageView(new Image(inputZebaStream));
 
 
     public static void showBirdsMedia() throws IOException {
@@ -55,31 +59,83 @@ public class BirdsMediaController  {
         Application.getMainStage().setScene(scene);
         Application.getMainStage().show();
     }
-    @FXML
-    void playKos(ActionEvent event){
-        File mediaFile = new File("src/main/resources/music/Tmerula.mp3");
-        playHitSound(mediaFile);
-    }
-    @FXML
-    void playSkanjac(ActionEvent event){
-        File mediaFile = new File("src/main/resources/music/Skanjac.mp3");
-        playHitSound(mediaFile);
-    }
-    @FXML
-    void playZeba(ActionEvent event){
-        File mediaFile = new File("src/main/resources/music/Zeba.mp3");
-        playHitSound(mediaFile);
-
-    }
 
     @FXML
-    void stopSoundAll(ActionEvent event){
+    void playKos(ActionEvent event) {
+        if (!isPlaying) {
+            isPlaying = true;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    File mediaFile = new File("src/main/resources/music/Tmerula.mp3");
+                    playHitSound(mediaFile);
 
-        mediaPlayer.stop();
+                    Platform.runLater(() -> {
+                        mediaPlayer.setOnPaused(() -> {
+                            synchronized (mediaPlayer) {
+                                mediaPlayer.notify();
+                            }
+                        });
+                    });
+                }
+            }).start();
+        }
+    }
+
+    @FXML
+    void playSkanjac(ActionEvent event) {
+        if (!isPlaying) {
+            isPlaying = true;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    File mediaFile = new File("src/main/resources/music/Skanjac.mp3");
+                    playHitSound(mediaFile);
+
+                    Platform.runLater(() -> {
+                        mediaPlayer.setOnPaused(() -> {
+                            synchronized (mediaPlayer) {
+                                mediaPlayer.notify();
+                            }
+                        });
+                    });
+                }
+            }).start();
+        }
 
     }
 
-    private void playHitSound(File mediaFile){
+    @FXML
+    void playZeba(ActionEvent event) {
+        if (!isPlaying) {
+            isPlaying = true;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    File mediaFile = new File("src/main/resources/music/Zeba.mp3");
+                    playHitSound(mediaFile);
+
+                    Platform.runLater(() -> {
+                        mediaPlayer.setOnPaused(() -> {
+                            synchronized (mediaPlayer) {
+                                mediaPlayer.notify();
+                            }
+                        });
+                    });
+                }
+            }).start();
+        }
+    }
+
+    @FXML
+    void stopSoundAll(ActionEvent event) {
+        if (isPlaying) {
+            isPlaying = false;
+            mediaPlayer.pause();
+        }
+    }
+
+    private void playHitSound(File mediaFile) {
         Media media = new Media(mediaFile.toURI().toString());
         mediaPlayer = new MediaPlayer(media);
         mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
@@ -87,13 +143,20 @@ public class BirdsMediaController  {
     }
 
     public void natragButtonClicked() throws IOException {
-        if (odabraniUser.equals("admin".toUpperCase())){
-            MainMenuController.showMainMenuScreen();
-            mediaPlayer.stop();
+        if (odabraniUser.equals("admin".toUpperCase())) {
+            try{
+                MainMenuController.showMainMenuScreen();
+                mediaPlayer.stop();
+            } catch (RuntimeException e){
+                logger.error("Uslo se u prozor te se nije nista pokrenulo od ponudjenog!");
+            }
         } else {
-            MainMenuUserController.showMainMenuScreenUser();
-            mediaPlayer.stop();
+            try{
+                MainMenuUserController.showMainMenuScreenUser();
+                mediaPlayer.stop();
+            }catch (RuntimeException e){
+                logger.error("Uslo se u prozor te se nije nista pokrenulo od ponudjenog!");
+            }
         }
     }
-
 }
